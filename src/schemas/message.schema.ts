e8dae0fa76c08311554b9f6702c4ec5267aba5bd
Trapzuss <nonthakorn.inthong@mail.kmutt.ac.th>
2022-06-27 19:09:03 +0700

    UPDATE chat schema

--- src/schemas/message.schema.ts
+++ src/schemas/message.schema.ts
@@ -5,16 +5,29 @@ export type MessagesDocument = Messages & Document;
 
 @Schema()
 export class Messages {
+  //   @Prop({ _id: true })
+  //   messageId: string;
+
   @Prop({ required: true, type: String, ref: 'User' })
   userId: string;
 
-  @Prop({ required: true, type: String, ref: 'Post' })
-  postId: string;
-
   @Prop({ required: true, type: String })
-  text: string;
+  messageText: string;
 
-  @Prop({ required: true, type: Date })
+  @Prop({
+    default: 'DEFAULT_MESSAGE',
+    enum: [
+      'DEFAULT_MESSAGE',
+      'CONFIRM_ADOPT',
+      'REQUEST_ADOPT',
+      'CANCEL_REQUEST_ADOPT',
+      'REJECT_ADOPT',
+    ],
+    type: String,
+  })
+  messageType: String;
+
+  @Prop({ default: Date.now(), type: Date })
   createdAt: Date;
 }
 
